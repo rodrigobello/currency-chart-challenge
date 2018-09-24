955f0f9d53b0e4dc968cc3b1427ce11b06eb96ea
Rodrigo Bello <rodrigoabbello@gmail.com>
2018-09-24 13:01:14 -0300

    Add 'get_dates_quotations' that make multi requests and 'create_quotation_dictionary' which treat each response

--- utils/api.py
+++ utils/api.py
@@ -40,3 +40,26 @@ def request_quotation(currency, date=None):
         return quotes['USDBRL']
 
     return quotes[f'USD{currency}'] / quotes['USDBRL']
+
+
+def get_dates_quotations(dates, currency):
+    """
+    :param dates: [ String ]
+    :param currency: String
+    :return: quotations as [ { date: String, currency: String, brl: Float } ]
+    """
+    return list(map(lambda x: create_quotation_dictionary(x, currency), dates))
+
+
+def create_quotation_dictionary(date, currency):
+    """
+    :param quote: Float
+    :param date: String
+    :param currency: String
+    :return: quotation as { date: String, currency: String, brl: Float }
+    """
+    return {
+        'date': date,
+        'quote': request_quotation(currency, date),
+        'currency': currency,
+    }
