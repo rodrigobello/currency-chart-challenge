7634e1ecc17e27e1d995b6d852f1601bdca89a6f
Rodrigo Bello <rodrigoabbello@gmail.com>
2018-09-26 05:01:41 -0300

    Add highcharts component to the currency chart render() method

--- client/src/components/Currency/CurrencyChart/CurrencyChart.js
+++ client/src/components/Currency/CurrencyChart/CurrencyChart.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ReactHighcharts from 'react-highcharts';
+import './CurrencyChart.css';
 
-const chart = ({ selectedCurrency, rates }) => (
-  <div className="chart">
-    <p>{`${selectedCurrency.name} Chart`}</p>
-    {rates.map(rate => <p key={rate.quote}>{rate.date}</p>)}
-  </div>
-);
+const chart = ({ selectedCurrency, rates }) => {
+  const config = {
+    yAxis: {
+      title: {
+        text: null,
+      },
+    },
+    xAxis: {
+      categories: [],
+    },
+    series: [{
+      name: `${selectedCurrency.id} to BRL`,
+      data: [],
+    }],
+  };
+
+  rates.reverse().forEach((rate) => {
+    config.xAxis.categories.push(rate.date);
+    config.series[0].data.push(rate.quote);
+  });
+
+  return (
+    <div className="chart">
+      <ReactHighcharts config={config} />
+    </div>
+  );
+};
 
 chart.propTypes = {
   selectedCurrency: PropTypes.shape({
